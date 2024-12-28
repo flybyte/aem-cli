1df800e66d44d1cb2fec365361416d636ca0f176
Frieder Heugel <frieder.heugel@gmail.com>
2024-12-28 14:33:23 +0100

    Update

--- src/constants.js
+++ src/constants.js
@@ -5,8 +5,13 @@ export const Constants = Object.freeze({
     },
     templates: {
         compose: "https://raw.githubusercontent.com/flybyte/aem-cli/refs/heads/main/resources/templates/compose.yml",
-        env: "https://raw.githubusercontent.com/flybyte/aem-cli/refs/heads/main/resources/templates/env.tpl",
+        env: "https://raw.githubusercontent.com/flybyte/aem-cli/refs/heads/main/resources/templates/env",
         mail: "https://raw.githubusercontent.com/flybyte/aem-cli/refs/heads/main/resources/templates/DefaultMailService.config",
         readme: "https://raw.githubusercontent.com/flybyte/aem-cli/refs/heads/main/resources/templates/README.md",
+        author: "https://raw.githubusercontent.com/flybyte/aem-cli/refs/heads/main/resources/templates/author.conf",
+        publish: "https://raw.githubusercontent.com/flybyte/aem-cli/refs/heads/main/resources/templates/publish.conf",
+        dispatcher: "https://raw.githubusercontent.com/flybyte/aem-cli/refs/heads/main/resources/templates/dispatcher.conf",
+        proxy: "https://raw.githubusercontent.com/flybyte/aem-cli/refs/heads/main/resources/templates/root.conf",
+        smtp: "https://raw.githubusercontent.com/flybyte/aem-cli/refs/heads/main/resources/templates/smtp.conf",
     },
 });
